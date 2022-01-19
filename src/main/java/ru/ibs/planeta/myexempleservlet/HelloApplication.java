9af7f22e1b32eb884f4d281067446ad4de9619af
Pavel Fadeev <PFadeev2@IBS.RU>
2022-01-19 20:48:51 +0300

    Первая версия

---
+++ src/main/java/ru/ibs/planeta/myexempleservlet/HelloApplication.java
@@ -0,0 +1,9 @@
+package ru.ibs.planeta.myexempleservlet;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
