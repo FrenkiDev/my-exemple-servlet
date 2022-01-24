a84c24e7f67631649f0161bc12b06b189ecf75c0
Pavel Fadeev <PFadeev2@IBS.RU>
2022-01-24 15:44:56 +0300

    Заверщен урок 7-9

--- src/main/java/ru/ibs/planeta/ServletAdd.java
+++ src/main/java/ru/ibs/planeta/ServletAdd.java
@@ -1,12 +1,17 @@
 package ru.ibs.planeta;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import ru.ibs.planeta.logic.Model;
 import ru.ibs.planeta.logic.User;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,8 +20,39 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ServletAdd extends HttpServlet {
     private final AtomicInteger counter = new AtomicInteger(5);
     Model model = Model.getInstance();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuffer bf = new StringBuffer();
+        String line;
+
+        try{
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null){
+                bf.append(line);
+            }
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        JsonObject jObject = gson.fromJson(String.valueOf(bf), JsonObject.class);
+
+        req.setCharacterEncoding("UTF-8");
+
+        String name = jObject.get("name").getAsString();
+        String surname = jObject.get("name").getAsString();
+        double salary = jObject.get("salary").getAsDouble();
+
+        User user = new User(name, surname, salary);
+        model.addUser(user, counter.getAndIncrement());
+
+        resp.setContentType("application/json;charset=utf-8");
+        PrintWriter pw = resp.getWriter();
+
+        pw.print(gson.toJson(model.getUserList()));
+    }
+
+    protected void doPostOld(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=utf-8");
         request.setCharacterEncoding("UTF-8");
         PrintWriter pw = response.getWriter();
